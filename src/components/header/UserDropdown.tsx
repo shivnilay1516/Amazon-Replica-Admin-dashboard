fdd8dff45195b60b09fd770ec3908333564d659f
shivnilay <shivnilay.srivastav@ens.enterprises>
2025-05-26 17:58:43 +0530

    logout token remove

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,21 +1,25 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import React, { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
+const router = useRouter();
+
+  const handellogout=()=>{
+    Cookies.remove("token")
+    router.push("/adminlogin")
+    console.log("logout")
+  }
 
 function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
   e.stopPropagation();
   setIsOpen((prev) => !prev);
 }
-
-const token = Cookies.get('token');
-console.log("token",token)
 
   function closeDropdown() {
     setIsOpen(false);
@@ -148,8 +152,8 @@ console.log("token",token)
             </DropdownItem>
           </li>
         </ul>
-        <Link
-          href="/adminlogin"
+        <button
+          onClick={handellogout}
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
         >
           <svg
@@ -168,7 +172,7 @@ console.log("token",token)
             />
           </svg>
           Sign out
-        </Link>
+        </button>
       </Dropdown>
     </div>
   );
